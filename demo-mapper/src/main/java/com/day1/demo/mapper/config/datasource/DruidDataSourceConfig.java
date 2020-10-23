1a725331824ecb0cb4c84ea8f99776efffb4ac12
Gorden <425166362@qq.com>
2020-10-23 18:42:33 +0800

    Druid数据源

--- demo-mapper/src/main/java/com/day1/demo/mapper/config/datasource/DruidDataSourceConfig.java
+++ demo-mapper/src/main/java/com/day1/demo/mapper/config/datasource/DruidDataSourceConfig.java
@@ -3,15 +3,20 @@ package com.day1.demo.mapper.config.datasource;
 import com.alibaba.druid.pool.DruidDataSource;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * @author: linhanghui
  * @Date: 2020/9/14 20:17
  * @Description:
  */
-@Data
-@ConfigurationProperties(prefix = "spring.datasource")
+@Configuration
 public class DruidDataSourceConfig {
 
-    private DruidDataSource druid;
+    @ConfigurationProperties(prefix = "spring.datasource")
+    private DataSource druid() {
+        return new DruidDataSource();
+    }
 }
